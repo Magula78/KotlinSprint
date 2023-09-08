e2809afa58ce3c38c6f7d45c3c914e634f3fd0e0
Magula78 <yevgenymagula@gmail.com>
2023-09-08 22:15:11 +0300

    KS_1-1 lesson 1 task 2

--- src/main/kotlin/lesson_1/lesson_1_task_2.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -1,18 +1,11 @@
 package lesson_1
 
 fun main(){
-    var quantityInOrder: Int
-    var totalOrder: Int
-    var art1Order: Int = 15
-    var art2Order: Int = 15
-    var art3Order: Int = 20
-    var art4Order: Int = 25
-    var productName: String
-    val art1: String = "dress"
-    val art2: String = "shoes"
-    val art3: String = "trousers"
-    val art4: String = "Jacket"
-    var price: Double
+    var quantityInOrder: Int=75
+    var dress: Int = 15
+    var shoes: Int = 15
+    var trousers: Int = 20
+    var jacket: Int = 25
     var textIsGrateful:String = "Thanks of the order"
     val quantityWorker: Int = 2000
     println("Количество рабочих интернет магазина составляет $quantityWorker")
