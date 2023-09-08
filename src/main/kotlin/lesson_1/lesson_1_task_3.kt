08e5e7292be45c86e11739cdba9d7848b394aba4
Magula78 <yevgenymagula@gmail.com>
2023-09-08 23:41:28 +0300

    KS_1-3 lesson 1 task 3

---
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -0,0 +1,16 @@
+package lesson_1
+
+fun main(){
+    val yearStart: Int = 1961
+    var hour:Int = 9
+    var minute:Int =7
+    println("Год взлета Гагарина Ю.А.:$yearStart")
+    println("час взлета Гагарина Ю.А.:0$hour часов")
+    println("час взлета Гагарина Ю.А.:0$minute минут")
+
+    hour= 10
+    minute =55
+    print("Гагарин Ю.А. приземлился в $hour:")
+    print("$minute")
+
+}
