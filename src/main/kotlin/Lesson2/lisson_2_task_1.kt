67c0704b6a0ea78ca6a81049ffee5d2acb2157ae
Magula78 <yevgenymagula@gmail.com>
2023-09-10 19:17:04 +0300

    KS_2-1 lesson 2 task 1

---
+++ src/main/kotlin/Lesson2/lisson_2_task_1.kt
@@ -0,0 +1,12 @@
+package Lesson2
+
+fun main() {
+    val balPerson1: Int = 3
+    val balPerson2: Int = 4
+    val balPerson3: Int = 3
+    val balPerson4: Int = 5
+    var summ: Int = balPerson1 + balPerson2 + balPerson3 + balPerson4
+    var averegeBal = summ.toDouble() / 4
+    println("Averege score in English of students: $averegeBal")
+
+}
