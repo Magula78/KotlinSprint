9e169612cab3123a8155ddd380242e2781d61153
Magula78 <yevgenymagula@gmail.com>
2023-09-10 04:46:57 +0300

    KS_1-5 lesson 1 task 5

--- src/main/kotlin/Lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/Lesson_1/lesson_1_task_5.kt
@@ -6,6 +6,7 @@ fun main(){
     val minutes:Byte = 48
     val second:Byte = 0
     val t:String =  String.format("%02d:%02d:%02d",hour,minutes,second)
+    print("Время полета Гагарина Ю.А. в космосе составляло: ")
     println(t)
 
 }
