9a6c67f8576b637be7aad64d7fd022756ae85efa
Magula78 <yevgenymagula@gmail.com>
2023-09-11 11:42:35 +0300

    KS_2-2 lesson 2 task 2

--- src/main/kotlin/lesson2/lesson_2_task_2.kt
+++ src/main/kotlin/lesson2/lesson_2_task_2.kt
@@ -1,15 +1,15 @@
 package lesson2
 
-fun main(){
+fun main() {
     var essentialWorkers: Int = 50
     var trainee: Int = 30
-    var totalEmployees:Int = essentialWorkers+trainee
-    var salaryEssentialWorker:Int = 30000
-    var salaryTrainee:Int = 20000
-    var selaryEssWorkers:Int = salaryEssentialWorker*essentialWorkers
-    var summSelaryTrainee:Int = salaryTrainee*trainee
-    var salaryCosts:Int =selaryEssWorkers+ summSelaryTrainee
-    var averageSalaryEmployees:Int=salaryCosts/totalEmployees
+    var totalEmployees: Int = essentialWorkers + trainee
+    var salaryEssentialWorker: Int = 30000
+    var salaryTrainee: Int = 20000
+    var selaryEssWorkers: Int = salaryEssentialWorker * essentialWorkers
+    var summSelaryTrainee: Int = salaryTrainee * trainee
+    var salaryCosts: Int = selaryEssWorkers + summSelaryTrainee
+    var averageSalaryEmployees: Int = salaryCosts / totalEmployees
     println("Всего работников на предприятии: $totalEmployees")
     println("Всего заработная плата постоянных сотрудников: $selaryEssWorkers рублей")
     println("Всего заработная плата стажеров: $summSelaryTrainee рублей")
