1a7691d9e27151ef360a8142485e8bdcc3aeae2e
Magula78 <yevgenymagula@gmail.com>
2023-09-11 12:43:21 +0300

    KS_2-2 lesson 2 task 2

--- src/main/kotlin/Lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/Lesson_1/lesson_1_task_5.kt
@@ -1,12 +1,11 @@
 package Lesson_1
 
-fun main(){
-    val seconds:Short = 6480
-    val hour:Byte = 1
-    val minutes:Byte = 48
-    val second:Byte = 0
-    val t:String =  String.format("%02d:%02d:%02d",hour,minutes,second)
-    print("Время полета Гагарина Ю.А. в космосе составляло: ")
-    println(t)
+fun main() {
+    val seconds: Int = 6480
+    val hour: Int = seconds / 3600
+    val minutes: Int = (seconds % 3600) / 60
+    val second: Int = seconds % 60
+    println("Время полета Гагарина Ю.А. в космосе составляло: ")
+    print(String.format("%02d:%02d:%02d", hour, minutes, second))
 
 }
