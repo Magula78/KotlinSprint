da77bf40b12fb538b05ddf9c6227cb770462379e
Magula78 <yevgenymagula@gmail.com>
2023-09-13 13:06:54 +0300

    KS_3-4 lesson 3 task 4

--- src/main/kotlin/lesson_3/lesson_3_task_4.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -1,14 +1,14 @@
 package lesson_3
 
-fun main (){
-    var moveFrom:String = "E2"
-    var moveWhere:String = "E4"
+fun main() {
+    var moveFrom: String = "E2"
+    var moveWhere: String = "E4"
     var numberMove = 1
 
     println("Игрок совершил ход № $numberMove c $moveFrom на $moveWhere")
 
     moveFrom = "D2"
-    moveWhere= "D3"
+    moveWhere = "D3"
     numberMove = 2
 
     println("Игрок совершил ход № $numberMove c $moveFrom на $moveWhere")
