b8d1671ea8972b38593b28b911e2591bbe962f1f
Magula78 <yevgenymagula@gmail.com>
2023-09-14 02:46:35 +0300

    KS_3-1 lesson 3 task 1

--- src/main/kotlin/Lesson3/lesson3_task1.kt
+++ src/main/kotlin/Lesson3/lesson3_task1.kt
@@ -1,10 +1,10 @@
 package Lesson3
 
 fun main() {
-    val privetDey: String = " Добрый день! Вас приветсвует команда Сбербанка!"
-    val privetNight: String = " Добрый вечер! Вас приветсвует команда Сбербанка!"
+    val privetDey: String = ", Добрый день! Вас приветсвует команда Сбербанка!"
+    val privetNight: String = ", Добрый вечер! Вас приветсвует команда Сбербанка!"
     var name: String = "Евгений"
 
-    println(name + "!" + privetDey)
-    println(name + "!" + privetNight)
+    println("$name$privetDey")
+    println("$name$privetNight")
 }
