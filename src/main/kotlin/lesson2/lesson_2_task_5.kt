851f725fb4f9ba7022b8dd0fe01633b267447542
Magula78 <yevgenymagula@gmail.com>
2023-09-14 04:27:32 +0300

    KS_2-5 lesson 2 task 5

---
+++ src/main/kotlin/lesson2/lesson_2_task_5.kt
@@ -0,0 +1,12 @@
+package lesson2
+
+import java.math.RoundingMode
+
+fun main() {
+    val sumContribution = 70000
+    val percent = 16.7
+    val termContribution = 20
+
+    val profit = String.format("%.3f", ((sumContribution * percent * termContribution) / 100) + sumContribution)
+    println(profit)
+}
