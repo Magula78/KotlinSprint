b687f7bf0fa57a1afc8c73e29addb3e07e37c4bc
Magula78 <yevgenymagula@gmail.com>
2023-09-16 19:31:16 +0300

    KS_4-2 lesson 4 task 2

--- src/main/kotlin/lesson_4/lesson_4_task_2.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_2.kt
@@ -3,22 +3,13 @@ package lesson_4
 fun main() {
     val cargo1 = 20
     val volume1 = 80
-    println(
-        "Груз с весом $cargo1 и обьемом $volume1 соответсвует категории Avarage: ${
-            (cargo1 >= MIN_MASS) &&
-                    (cargo1 <= MAX_MASS) && (volume1 < VOLUM)
-        }"
-    )
+    println("Груз с весом $cargo1 и обьемом $volume1 соответсвует категории Avarage: ${(cargo1 >= MIN_MASS) && 
+            (cargo1 <= MAX_MASS) && (volume1 < VOLUM)}")
 
     val cargo2 = 50
     val volume2 = 100
-    println(
-        "Груз с весом $cargo2 и обьемом $volume2 соответсвует категории Avarage: ${
-            (cargo2 >= MIN_MASS) &&
-                    (cargo2 <= MAX_MASS) && (volume2 < VOLUM)
-        }"
-    )
-}
+    println("Груз с весом $cargo2 и обьемом $volume2 соответсвует категории Avarage: ${(cargo2 >= MIN_MASS) && 
+            (cargo2 <= MAX_MASS) && (volume2 < VOLUM)}")}
 
 const val MAX_MASS = 100
 const val MIN_MASS = 35
