31006e6cdde7c45bc67637d2e9fe4ca20a8022ed
Magula78 <yevgenymagula@gmail.com>
2023-09-18 00:10:12 +0300

    KS_5-2 lesson 5 task 2

---
+++ src/main/kotlin/lesson_5/lesson_5_task_2.kt
@@ -0,0 +1,13 @@
+package lesson_5
+
+fun main() {
+    println("Введите год рождения пользователя:")
+    val userYearBorn = readLine()!!.toInt()
+
+    val userAge = THIS_YEAR - userYearBorn
+
+    if (userAge >= AGE_OF_MAJORITY) {println("Показывать экран со скрытым контентом")}
+    else { println("Показвать экран с ограниченным контентом")}
+}
+const val AGE_OF_MAJORITY = 18
+const val THIS_YEAR = 2023
