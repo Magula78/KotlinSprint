220a71424e1cb728065400ab0584a85ebfb12c1e
Magula78 <yevgenymagula@gmail.com>
2023-09-19 21:33:29 +0300

    KS_5-1 lesson 5 task 1

--- src/main/kotlin/lesson5/lesson_5_task_1.kt
+++ src/main/kotlin/lesson5/lesson_5_task_1.kt
@@ -2,11 +2,14 @@ package lesson5
 
 fun main() {
     println("Здравствуйте! Вход в ситему!")
-    println("Автоизация. Если вы не БОТ решите простой пример:")
-    println("1+1 = ?")
+    println("Автоизация. Если вы не БОТ решите простой пример на сложение:введите последовательно два числа и дайте ответ")
+    println("Введите первое число")
+    var number1 = readLine()!!.toInt()
+    println("Введите второе число")
+    var number2 = readLine()!!.toInt()
     println("Введите ответ:")
-
     var ansver = readLine()!!.toInt()
+    var sum = number1 + number2
 
-    if (ansver == 2) println("Добро пожаловать!") else println("Доступ запрещен")
+    if (ansver == sum) println("Добро пожаловать!") else println("Доступ запрещен")
 }
