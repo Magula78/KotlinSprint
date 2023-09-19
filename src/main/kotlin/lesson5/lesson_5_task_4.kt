e7e133eb56eee71ab0a51ff39cf6d8d3cfef6c96
Magula78 <yevgenymagula@gmail.com>
2023-09-19 09:20:24 +0300

    KS_4-5 lesson 4 task 5

---
+++ src/main/kotlin/lesson5/lesson_5_task_4.kt
@@ -0,0 +1,25 @@
+package lesson5
+
+fun main(){
+
+    println("Добро пожаловать. Вас приветсвует робот -андроид Марвин")
+    println("Для Вашей верификации, введите свои данные")
+
+    println("Введите ЛОГИН")
+    val login = readln()
+
+    println("Введите ПОРОЛЬ")
+    val porol = readln()
+
+    if (login == LOGIN){}
+    if (porol == POROL) {println("Ваши данные проверены, входите в корабль HEART OF GOLD")
+    }else {println("Вы вели не верные данные. Пройдите пожайлуста регистрацию тут")
+        println("Придумайте ЛОГИН из букв и введите:")
+        val login2 = readln()
+        println("Придумайте ПОРОЛЬ из букв и введите:")
+        val porol2 = readln()
+        println("Спасибо $login2 запомните ваш пороль $porol2, регистрация завершина - проходите на корабль HEART OF GOLD")
+        }
+    }
+const val LOGIN= "Zaphod"
+const val POROL= "PanGalactic"
