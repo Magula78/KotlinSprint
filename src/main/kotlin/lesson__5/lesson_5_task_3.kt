500758ea24a6b29f2f91cb60ed17a1b72cc657e8
Magula78 <yevgenymagula@gmail.com>
2023-09-20 23:38:07 +0300

    KS_5-3 lesson 5 task 3

--- src/main/kotlin/lesson__5/lesson_5_task_3.kt
+++ src/main/kotlin/lesson__5/lesson_5_task_3.kt
@@ -5,24 +5,15 @@ fun main() {
     println("Введите по запросу два любых числа в диапозоне от 1 до 100")
 
     println("Введите первое число")
-    val attempt1 = readLine()!!.toInt()
-    if ((attempt1 != ANSVER_1) && (attempt1 != ANSVER_2)) {println("Вы не угадали первое число")
-    } else if (attempt1 == ANSVER_1 || attempt1 == ANSVER_2) {"Поздравляю вы угадали первое число" }
-
-    val corect1 = ANSVER_1 - attempt1
-    val corect2 = ANSVER_2 - attempt1
-
-    println("Введите второе число")
-    val attempt2 = readLine()!!.toInt()
-    if ((attempt2 != ANSVER_1) && (attempt2 != ANSVER_2)) {println("Вы не угадали второе число") }
-    else if (attempt2 == ANSVER_1 || attempt2 == ANSVER_2) {"Поздравляю вы угадали второе число" }
-
-    val corect3 = ANSVER_1 - attempt2
-    val corect4 = ANSVER_2 - attempt2
-
-    if ((corect1 == 0 || corect2 == 0) && (corect3 == 0 || corect4 == 0)) { println("Поздравляю вы выйграли главный приз!")
-    } else if (corect1 != 0 && corect2 != 0 && corect3 != 0 && corect4 != 0) {println("неудача!")
-    } else {println("Утешительный приз!")}
+    val attempt1 = readLine()?.toInt() ?: 0
+    val attempt2 = readLine()?.toInt() ?: 0
+    if ((attempt1 == ANSVER_1 || attempt1 == ANSVER_2) && (attempt2 == ANSVER_1 || attempt2 == ANSVER_2)) {
+        println("Поздравляю вы выйграли главный приз!")
+    } else if ((attempt1 != ANSVER_1 && attempt1 != ANSVER_2) && (attempt2 != ANSVER_1 && attempt2 != ANSVER_2)) {
+        println("неудача!")
+    } else {
+        println("Утешительный приз!")
+    }
 
 }
 
