3f4e6140197c40ee3e8ce92f2a45c484d45d2b57
Magula78 <yevgenymagula@gmail.com>
2023-09-25 14:00:49 +0300

    KS_7-2 lesson 7 task 2

--- src/main/kotlin/lesson_7/lesson_7_task_2.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_2.kt
@@ -5,7 +5,7 @@ import kotlin.random.Random
 fun main() {
     for (i in 1..1000) {
         println("Это код из СМС сообщения:")
-        var sms = Random.nextInt(1, 9)
+        var sms = Random.nextInt(1000, 9999)
         println(sms)
         println("Введите СМС в программу авторизации:")
         var ansverSms = readLine()?.toInt() ?: 0
