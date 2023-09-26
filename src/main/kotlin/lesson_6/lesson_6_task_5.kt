70bc57e3639652e0abf31364a7a94cc6bcd34517
Magula78 <yevgenymagula@gmail.com>
2023-09-26 14:39:58 +0300

    KS_6-5 lesson 6 task 5

--- src/main/kotlin/lesson_6/lesson_6_task_5.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_5.kt
@@ -8,34 +8,28 @@ fun main() {
     var randomNum1 = Random.nextInt(1, 9)
     var randomNum2 = Random.nextInt(1, 9)
     println("Решите пример: $randomNum1 + $randomNum2")
-    var result1 = randomNum1+randomNum2
-    var num = 1
-    println("Попытка: $num")
+    var result = randomNum1 + randomNum2
+    var attempt = 1
+    println("Попытка: $attempt")
     println("Введите ответ:")
     var ansver = readLine()?.toInt() ?: 0
 
-
-    var nu1 = ansver
-
-    while ((ansver != result1) && (num < TRY)) {
-        num = num + 1
+    while ((ansver != result) && (attempt < 3)) {
+        attempt = attempt + 1
         println("Неправильный ответ")
-        println("Попытка: $num")
+        println("Попытка: $attempt")
         randomNum1 = Random.nextInt(1, 9)
         randomNum2 = Random.nextInt(1, 9)
         println("Решите пример: $randomNum1 + $randomNum2")
-        result1 = randomNum1+randomNum2
+        result = randomNum1 + randomNum2
         println("Введите ответ:")
         ansver = readLine()?.toInt() ?: 0
     }
 
-    var nu2 = ansver
-
-    if ((nu1 == result1) || (nu2 == result1)) {
+    if ((ansver == result)) {
         println("Добро пожаловать!")
     } else {
         println("Доступ запрещен")
     }
 
 }
-const val TRY = 3
