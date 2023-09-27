9a75eb8362edd90adb2e1d47cabd431bff6d73d8
Magula78 <yevgenymagula@gmail.com>
2023-09-27 13:41:42 +0300

    KS_8-2 lesson 8 task 2

---
+++ src/main/kotlin/lesson_8/lesson_8_task_2.kt
@@ -0,0 +1,15 @@
+package lesson_8
+
+fun main() {
+    val indigrientsSup = arrayListOf("вода", "кортошка", "макароны", "соль", "лук", "морковь", "зелень", "масло")
+    println("Введите название индигриента в блюде:")
+    val findIndigrient = readln()
+
+    for (i in indigrientsSup) {
+        if (i == findIndigrient) {
+            println("Индигриент ($findIndigrient) в рицепте блюда есть")
+            return
+        }
+    }
+    println("Такого индигриента нет.")
+}
