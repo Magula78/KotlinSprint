0af22ed72202346531d3e3fba625a90e2e7d5e99
Magula78 <yevgenymagula@gmail.com>
2023-10-01 14:19:34 +0300

    KS_8-4 lesson 8 task 4

---
+++ src/main/kotlin/lesson_9/lesson_9_task_1.kt
@@ -0,0 +1,11 @@
+package lesson_9
+
+fun main() {
+    val indigrientsSup = listOf("вода", "кортошка", "макароны", "соль", "лук", "морковь", "зелень", "масло")
+    println("Ингредиенты блюдa: ")
+
+    for (i in indigrientsSup) {
+        println(i)
+    }
+
+}
