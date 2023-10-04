f19877902365fa754e81c73d7a263a0fc9dc235e
Magula78 <yevgenymagula@gmail.com>
2023-10-04 11:36:26 +0300

    KS_9-4 lesson 9 task 4

--- src/main/kotlin/lesson_9/lesson_9_task_4.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_4.kt
@@ -2,12 +2,12 @@ package lesson_9
 
 fun main() {
 
-    println("Введите пять ингредиентов блюда в одной стокеБ через запятую без пробелов")
+    println("Введите пять ингредиентов блюда в одной стоке через запятую без пробелов")
     val dataInput = readLine().toString()
 
-    val listFoods: List<String> = listOf(*dataInput.split(",").toTypedArray())
+    val sortedListFoods = dataInput.split(",").sorted()
 
-    val sortedListFoods = listFoods.sorted()
     println(sortedListFoods)
+
 
 }
