7b2468555183555daa184cbbc91ff1bff2b8a170
Magula78 <yevgenymagula@gmail.com>
2023-10-04 12:24:59 +0300

    KS_9-2 lesson 9 task 2

--- src/main/kotlin/lesson_9/leccon_9_task_2.kt
+++ src/main/kotlin/lesson_9/leccon_9_task_2.kt
@@ -3,16 +3,17 @@ package lesson_9
 fun main() {
     val indigrientsSup = mutableListOf("вода", "кортошка", "макароны")
     println("Блюдо имеет базовые ингредиенты:$indigrientsSup")
-    println("Желаете  добавить еще индигриенты?")
-    var ansver = readLine()
-    if (ansver != "Да" && ansver != "да") {
-        return
-    }
-    if (ansver == "Да" || ansver == "да") {
+    println("Желаете  добавить еще ингредиенты?")
+    var ansver = readLine().toString()
+
+    if (ansver.contains("да", ignoreCase = true)) {
         println("Какой ингредиент вы хотите добваить ?")
         var foodNew = readln()
         indigrientsSup.add(foodNew)
         println("Теперь в блюде есть следующие ингредиенты:$indigrientsSup")
+    } else {
+        return
     }
+
 
 }
