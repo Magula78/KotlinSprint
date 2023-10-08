28279759926649869a044f9de940701ee774a452
Magula78 <yevgenymagula@gmail.com>
2023-10-08 23:56:05 +0300

    KS_10-3 lesson 10 task 3

--- src/main/kotlin/lesson_10/lesson_10_task_3.kt
+++ src/main/kotlin/lesson_10/lesson_10_task_3.kt
@@ -13,14 +13,13 @@ fun generationPassword() {
     val random = Random
     val symdols = "${'A'..'Z'}" + "${'a'..'z'}" + "123456789" + "!#$%'()*+,-./" + "" + " "
     val bulder = StringBuilder()
-    println(symdols.random())
+
 
     for (i in 0 until lengthPassword) {
         bulder.append(symdols[random.nextInt(symdols.length)])
 
     }
     println(bulder)
-    val randomString = bulder.toString()
-    println(randomString)
+
 
 }
