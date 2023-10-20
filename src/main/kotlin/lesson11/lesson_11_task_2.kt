839c843dbe0ada7d0509bd2015276ab3539c5104
Magula78 <yevgenymagula@gmail.com>
2023-10-20 20:20:13 +0300

    KS_11-2 lesson 11 task 2

--- src/main/kotlin/lesson11/lesson_11_task_2.kt
+++ src/main/kotlin/lesson11/lesson_11_task_2.kt
@@ -1,55 +1,50 @@
 package lesson11
 
 class User2(
-    var id: Int,
-    var name: String,
+    val id: Int,
+    val name: String,
     var password: String,
-    var mail: String,
-    var bio: String,
+    val mail: String,
+    var bio: String = "",
 )
 
 fun main() {
-    var user1: User2 = User2(
+    val user1: User2 = User2(
         id = 1,
         name = "Вова",
         password = "tttt",
         mail = "Vova@mail.ru",
-        bio = ""
-    )
-    inputInformation(user1)
-    forBio(user1)
-    changePassword(user1)
-    inputInformation(user1)
 
-    var user3: User2 = User2(
+        )
+    displayInformation(user1)
+    collectionInformationBio(user1)
+    changePassword(user1)
+    displayInformation(user1)
+
+    val user3: User2 = User2(
         id = 3,
         name = "Yevgen",
         password = "aaaa",
         mail = "yebrend@mail.ru",
-        bio = ""
-    )
+
+        )
     println()
-    inputInformation(user3)
-    forBio(user3)
+    displayInformation(user3)
+    collectionInformationBio(user3)
     changePassword(user3)
-    inputInformation(user3)
+    displayInformation(user3)
 
 }
 
-fun inputInformation(user1: User2) {
+fun displayInformation(user1: User2) {
     println()
     println("Информация о пользователе:")
-    println("ID номер:  ${user1.id}")
-    println("Имя пользователя:  ${user1.name}")
-    println("Пароль:  ${user1.password}")
-    println("Email:  ${user1.mail}")
-    println("BIO information:  ${user1.bio}")
+    println("ID номер:${user1.id},имя:${user1.name}, пароль:${user1.password},email:${user1.mail}")
 }
 
-fun forBio(user1: User2) {
+fun collectionInformationBio(user1: User2) {
 
     user1.bio = "${user1.id},${user1.name},${user1.password},${user1.mail}"
-
     println(user1.bio)
 }
 
@@ -57,11 +52,11 @@ fun changePassword(user1: User2) {
     println()
     println("Изменение пароля:")
     println("Введите текущий пароль")
-    var oldPassword = readln()
+    val oldPassword = readln()
     if (oldPassword == user1.password) {
         println("Старый пароль введен верно")
         println("Введите новый пароль")
-        var newPassword = readln()
+        val newPassword = readln()
         user1.password = newPassword
         user1.bio = "ID номер:${user1.id}, имя:${user1.name}, парооль:${user1.password}, email adres:${user1.mail}"
         println("Пароль пользлователя ${user1.name} - изменен")
