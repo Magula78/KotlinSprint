3c08db9c952475c0ed22cf947c7e4c79d5c130aa
Magula78 <yevgenymagula@gmail.com>
2023-10-25 16:47:06 +0300

    KS_11-3 lesson 11 task 3

--- src/main/kotlin/lesson_11/lesson_11_task_3.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_3.kt
@@ -3,59 +3,93 @@ package lesson_11
 class CardRoomInterest(
     var cover: String,
     var nameRoom: String,
-    var listOfParticipats: MutableList<AvatarUser>,
-)
+    var participants: MutableList<AvatarUser>
+) {
+    fun coverIt(participants: MutableList<AvatarUser>): MutableList<AvatarUser> {
+        println("ЛЕНТА")
+        println("В ЭТОЙ ЛЕНТЕ СЛЕДУЮЩИЕ КОМНАТЫ ПО ИНТЕРЕСАМ:")
+        println("ОБЛОЖКА КОМНАТЫ:${cover}")
+        println("НАЗВАНИЕ КОМНАТЫ: ${nameRoom}")
+        for (i in 0..participants.size - 1) {
+            println("${i + 1}-й УЧАСТНИК КОМНАТЫ ''${nameRoom}'':${participants[i].name}")
+        }
+        return participants
+    }
+}
 
 class AvatarUser(
-    var name: String,
-    var talking: Boolean,
-    var microphoneMuted: Boolean,
-    var userIsMuted: Boolean,
-)
+    var name: String = "",
+    var talking: Boolean = false,
+    var microphoneMuted: Boolean = false,
+    var userIsMuted: Boolean = false,
+) {
+
+    fun longPressAvatar(participants: MutableList<AvatarUser>) {
+
+        println("Долгое нажатие на аватар:")
+        println("Выбери имя участника комнаты.Введите имя")
+
+        var choiceAvatar = readln()
+        for (i in 0..participants.size - 1)
+
+            if (participants[i].name.contains(choiceAvatar) == true) {
+
+                if (choiceAvatar == participants[i].name) {
+                    println("НИКНЕЙМ АВАТАРА:  ${participants[i].name}")
+                    when {
+                        (participants[i].talking == true) -> println("БЕЙДЖ. СТАТУС: ${participants[i].name} - разговаривает")
+                        (participants[i].microphoneMuted == true) -> println("БЕЙДЖ. СТАТУС: ${participants[i].name} - микрофон выключен")
+                        (participants[i].userIsMuted == true) -> println("БЕЙДЖ. СТАТУС: ${participants[i].name} - пользователь заглушен")
+                    }
+
+                } else {
+                    println("Не достаточно долгое нажатие на Аватарке участника (Имя участника введено не корректно).")
+                }
+            }
+    }
+}
 
 fun main() {
-    longPressAvatar()
-}
 
-
-fun longPressAvatar() {
-
-    var roomInterest: CardRoomInterest = CardRoomInterest(
-        cover = "Drawing with a mug of beer",
-        nameRoom = "Beer lovers club",
-        listOfParticipats = mutableListOf(
-            AvatarUser("Alex", true, false, false),
-            AvatarUser("Tom", false, false, true),
-            AvatarUser("Thomas", false, true, false)
-        ),
+    var listParticipantsВeer = mutableListOf<AvatarUser>(
+        AvatarUser("Tod", true, false, false),
+        AvatarUser("Bob", false, true, false),
+        AvatarUser("Pit", false, false, true),
+    )
+    var roomInterestBeer = CardRoomInterest(
+        cover = "На обложке изображен стакан с пивом",
+        nameRoom = "Пивнушка",
+        participants = listParticipantsВeer
     )
 
-    println("ЛЕНТА")
-    println("В ЭТОЙ ЛЕНТЕ СЛЕДУЮЩИЕ КОМНАТЫ ПО ИНТЕРЕСАМ:")
-    println("ОБЛОЖКА КОМНАТЫ:${roomInterest.cover}, НАЗВАНИЕ КОМНАТЫ: ${roomInterest.nameRoom}")
-    println("УЧАСТНИКИ КОМНАТЫ:${roomInterest.listOfParticipats[0].name},${roomInterest.listOfParticipats[1].name},${roomInterest.listOfParticipats[2].name}")
+    roomInterestBeer.coverIt(listParticipantsВeer)
 
-    println("Долгое нажатие на аватар (Введите имя АВАТАР-участника):")
-    var listOfParticipatsSize = roomInterest.listOfParticipats.size
-    var choiceAvatar = readln()
-    for (i in 0..listOfParticipatsSize - 1)
+    for (i in 0..roomInterestBeer.participants.size - 1) {
+        roomInterestBeer.participants[i].longPressAvatar(listParticipantsВeer)
+    }
 
-        if (roomInterest.listOfParticipats[i].name.contains(choiceAvatar) == true) {
+    var listParticipantsHoney = mutableListOf<AvatarUser>(
+        AvatarUser("Ivan", true, false, false),
+        AvatarUser("Alex", false, true, false),
+        AvatarUser("Duke", false, false, true),
+        AvatarUser("Jone", false, false, true),
+    )
 
-            if (choiceAvatar == roomInterest.listOfParticipats[i].name) {
-                println("НИКНЕЙМ АВАТАРА:  ${roomInterest.listOfParticipats[i].name}")
-                if (roomInterest.listOfParticipats[i].talking == true) {
-                    println("БЕЙДЖ. СТАТУС: ${roomInterest.listOfParticipats[i].name} - разговаривает")
-                }
-                if (roomInterest.listOfParticipats[i].microphoneMuted == true) {
-                    println("БЕЙДЖ. СТАТУС: ${roomInterest.listOfParticipats[i].name} - микрофон выключен")
-                }
-                if (roomInterest.listOfParticipats[i].userIsMuted == true) {
-                    println("БЕЙДЖ. СТАТУС: ${roomInterest.listOfParticipats[i].name} - пользователь заглушен")
-                }
+    var roomInterestHoney = CardRoomInterest(
+        cover = "На обложке изображена бочка с медом",
+        nameRoom = "Пасечник",
+        participants = listParticipantsHoney
+    )
 
-            } else {
-                println("Не достаточно долгое нажатие на Аватарке участника (Имя участника введено не корректно).")
-            }
-        }
+    roomInterestHoney.coverIt(listParticipantsHoney)
+
+    for (i in 0..roomInterestHoney.participants.size - 1) {
+        roomInterestHoney.participants[i].longPressAvatar(listParticipantsHoney)
+    }
 }
+
+
+
+
+
+
