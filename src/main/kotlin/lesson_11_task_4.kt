194aa72da4cc16d7d41029e9911206a117fec28c
Magula78 <yevgenymagula@gmail.com>
2023-11-10 09:21:06 +0300

    KS_11-5 lesson 11 task 5

--- src/main/kotlin/lesson_11_task_4.kt
+++ src/main/kotlin/lesson_11_task_4.kt
@@ -3,49 +3,39 @@ package lesson_11
 class Store(
     val coverStore: String,
     val nameStore: String,
-    var listWindowsStore: MutableList<WindowsStore>
+    val listWindowsStore: MutableList<WindowsStore>
 )
 
 class WindowsStore(
     val coverWindows: String,
     val nameWindow: String,
-    var listCategoryWindow: MutableList<CategoryDishes> = mutableListOf(),
-    var listFavoriteDishesWindow: MutableList<Dishes> = mutableListOf(),
+    val listCategoryWindow: MutableList<CategoryDishes> = mutableListOf(),
+    val listFavoriteDishesWindow: MutableList<Dishes> = mutableListOf(),
 ) {
 
     fun addInFavoriteDishes(listFavoriteDishesWindow: MutableList<Dishes>) {
-        var favoriteDishes: MutableList<Dishes> = mutableListOf<Dishes>()
         println(nameWindow)
         println(coverWindows)
         if (listFavoriteDishesWindow.size == 0) {
             println("категория ${nameWindow} пуста")
         } else {
             for (i in 0..listFavoriteDishesWindow.size - 1) {
-                favoriteDishes.add(listFavoriteDishesWindow[i])
+                listFavoriteDishesWindow.add(listFavoriteDishesWindow[i])
                 println(listFavoriteDishesWindow)
             }
         }
     }
 
     fun addCategoryDishes(listCategoryWindow: MutableList<CategoryDishes>) {
-        var availableDishCategories: MutableList<CategoryDishes> = mutableListOf<CategoryDishes>()
         println(nameWindow)
         println(coverWindows)
         for (i in 0..listCategoryWindow.size - 1) {
-            availableDishCategories.add(listCategoryWindow[i])
+            listCategoryWindow.add(listCategoryWindow[i])
             println(listCategoryWindow[i].name)
         }
     }
 
-    fun addDishes1(diversityDish: MutableList<Dishes>) {
-        var availableDishs: MutableList<Dishes> = mutableListOf<Dishes>()
-        println(nameWindow)
-        println(coverWindows)
-        for (i in 0..diversityDish.size - 1) {
-            availableDishs.add(diversityDish[i])
-            println(diversityDish[i].name)
-        }
-    }
+
 }
 
 class CategoryDishes(
@@ -55,11 +45,10 @@ class CategoryDishes(
     var inFavorites: Boolean = false,
 ) {
     fun addDishes(diversityDish: MutableList<Dishes>) {
-        var availableDishs: MutableList<Dishes> = mutableListOf<Dishes>()
         println(name)
         println(cover)
         for (i in 0..diversityDish.size - 1) {
-            availableDishs.add(diversityDish[i])
+            diversityDish.add(diversityDish[i])
             println(diversityDish[i].name)
         }
     }
@@ -75,36 +64,36 @@ class Dishes(
 
 fun main() {
 
-    var burger1: Dishes = Dishes("", "бургер1", mutableListOf("мука", "вода", "соль"))
-    var burger2: Dishes = Dishes("", "чизбургер", mutableListOf("мука", "вода", "соль"))
-    var burger3: Dishes = Dishes("", "бургер с грибами", mutableListOf("мука", "вода", "грибы", "соль"))
-    var pizza1: Dishes = Dishes("", "пицца 1", mutableListOf("мука", "вода", "соль"))
-    var pizza2: Dishes = Dishes("", "пицца 2", mutableListOf("мука", "вода", "соль"))
-    var pizza3: Dishes = Dishes("", "пицца 3", mutableListOf("мука", "вода", "соль"))
-    var dessert1: Dishes = Dishes("", "десерт1", mutableListOf("сахар", "мука", "вода", "соль"))
-    var dessert2: Dishes = Dishes("", "десерт2", mutableListOf("сахар", "мука", "вода", "соль"))
-    var dessert3: Dishes = Dishes("", "десерт3", mutableListOf("сахар", "мука", "вода", "соль"))
-    var fish1: Dishes = Dishes("", "рыба1", mutableListOf("рыба", "мука", "вода", "соль"))
-    var fish2: Dishes = Dishes("", "рыба2", mutableListOf("рыба", "мука", "вода", "соль"))
-    var fish3: Dishes = Dishes("", "рыба3", mutableListOf("рыба", "мука", "вода", "соль"))
+    val burger1: Dishes = Dishes("", "бургер1", mutableListOf("мука", "вода", "соль"))
+    val burger2: Dishes = Dishes("", "чизбургер", mutableListOf("мука", "вода", "соль"))
+    val burger3: Dishes = Dishes("", "бургер с грибами", mutableListOf("мука", "вода", "грибы", "соль"))
+    val pizza1: Dishes = Dishes("", "пицца 1", mutableListOf("мука", "вода", "соль"))
+    val pizza2: Dishes = Dishes("", "пицца 2", mutableListOf("мука", "вода", "соль"))
+    val pizza3: Dishes = Dishes("", "пицца 3", mutableListOf("мука", "вода", "соль"))
+    val dessert1: Dishes = Dishes("", "десерт1", mutableListOf("сахар", "мука", "вода", "соль"))
+    val dessert2: Dishes = Dishes("", "десерт2", mutableListOf("сахар", "мука", "вода", "соль"))
+    val dessert3: Dishes = Dishes("", "десерт3", mutableListOf("сахар", "мука", "вода", "соль"))
+    val fish1: Dishes = Dishes("", "рыба1", mutableListOf("рыба", "мука", "вода", "соль"))
+    val fish2: Dishes = Dishes("", "рыба2", mutableListOf("рыба", "мука", "вода", "соль"))
+    val fish3: Dishes = Dishes("", "рыба3", mutableListOf("рыба", "мука", "вода", "соль"))
 
-    var burgerDish = CategoryDishes("фото аппетитного бургера", "БУРГЕРЫ", mutableListOf(burger1, burger2, burger3))
-    var pizzaDish =
+    val burgerDish = CategoryDishes("фото аппетитного бургера", "БУРГЕРЫ", mutableListOf(burger1, burger2, burger3))
+    val pizzaDish =
         CategoryDishes("фото большой пиццы на деревянном блюде", "ПИЦЦА", mutableListOf(pizza1, pizza2, pizza3))
-    var fishDish = CategoryDishes("Фото жаренной рыбы на блюде", "РЫБА", mutableListOf(fish1, fish2, fish3))
-    var dessertDish = CategoryDishes("Фото пирожных", "ДЕСЕРТЫ", mutableListOf(dessert1, dessert2, dessert3))
+    val fishDish = CategoryDishes("Фото жаренной рыбы на блюде", "РЫБА", mutableListOf(fish1, fish2, fish3))
+    val dessertDish = CategoryDishes("Фото пирожных", "ДЕСЕРТЫ", mutableListOf(dessert1, dessert2, dessert3))
 
-    var windowsCategoryDishes = WindowsStore(
+    val windowsCategoryDishes = WindowsStore(
         coverWindows = "Обложка:Фото разнообразных блюд",
         nameWindow = "ОКНО : КАТЕГОРИИ",
         listCategoryWindow = mutableListOf<CategoryDishes>(burgerDish, pizzaDish, fishDish, dessertDish),
     )
-    var windowsFavoriteDishes = WindowsStore(
+    val windowsFavoriteDishes = WindowsStore(
         coverWindows = "Обложка:Фото самых популярных блюд",
         nameWindow = "ОКНО : ИЗБРАННОЕ",
         listFavoriteDishesWindow = mutableListOf<Dishes>(),
     )
-    var recipesStore = Store(
+    val recipesStore = Store(
         coverStore = "Надпись на фоне с названием Recipes",
         nameStore = "Recipes",
         listWindowsStore = mutableListOf<WindowsStore>(windowsCategoryDishes, windowsFavoriteDishes),
@@ -130,7 +119,6 @@ fun guide(
     println("Выйти из Recipes - введите в консоль 0")
     var key: Int = readln().toInt()
 
-    // windowsStore.addCategoryDishes()
     when {
         (key == 0) -> return
         (key == 1) -> {
